c65fb8838292cc704fdce3be09bf93fbcd0debaa
atharv <atharv55215@gmail.com>
2025-11-06 19:12:11 +0530

    Adding basic project structure

---
+++ src/test/java/com/pm/personalDiary/PersonalDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.personalDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
